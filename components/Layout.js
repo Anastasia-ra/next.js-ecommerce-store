6c674bbfb7bed9000dbb5fd82072adc682846daf
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-02-02 17:51:11 +0100

    Add favicon

--- components/Layout.js
+++ components/Layout.js
@@ -6,6 +6,9 @@ export default function Layout(props) {
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/icon-apple-touch.png" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
 
       <Header />
