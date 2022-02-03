e818644310d4fd21741bd4e185649e9f1dc0c053
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-02-03 14:42:12 +0100

    Add product database

---
+++ util/database.js
@@ -0,0 +1,42 @@
+const productsDatabase = [
+  {
+    id: '1',
+    name: 'Antarctic Expedition',
+    type: 'Cruise',
+    duration: '20',
+    price: 50,
+    description: `Let's see some penguins`,
+  },
+  {
+    id: '2',
+    name: 'Himalaya Trek',
+    type: 'Hike',
+    duration: '14',
+    price: 20,
+    description: `Let's see some yaks`,
+  },
+  {
+    id: '3',
+    name: 'Ultimate Moon Hike',
+    type: 'Hike',
+    duration: '50',
+    price: 400,
+    description: `The best view you've ever seen and amazing wildlife!`,
+  },
+  {
+    id: '4',
+    name: 'In the jungles of Brazil',
+    type: 'Hike',
+    duration: '10',
+    price: 30,
+    description: `I hope there are no crocodiles`,
+  },
+  {
+    id: '5',
+    name: 'Submarine tour of the earth',
+    type: 'Cruise',
+    duration: '70',
+    price: 250,
+    description: 'At least no one will bother you',
+  },
+];
