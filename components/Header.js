4ed00b318b6692c67c208489e0d20d71e12781cf
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-02-06 09:50:49 +0100

    Style header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,17 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { css } from '@emotion/react';
+
+const headerStyle = css`
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  background-color: #6497bb;
+`;
 
 export default function Header() {
   return (
-    <header>
+    <header css={headerStyle}>
       <Image src="/logo.png" width="150" height="56" />
       <Link href="/">
         <a>Home</a>
