46516d9f38de54c1a65a0fe4589b4df8c0c4c598
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-02-06 09:50:20 +0100

    Add background image to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
 import Head from 'next/head';
 import Layout from '../components/Layout';
+import { css } from '@emotion/react';
+import backgroundImage from '../public/background-homepage2.jpg';
+
+const layoutStyle = css`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background-image: url(/background-homepage2.jpg);
+  background-size: cover;
+  background-position: top 20px;
+`;
 
 export default function Home() {
   return (
-    <Layout>
-      <Head>
-        <title>Homepage</title>
-      </Head>
+    <div css={layoutStyle}>
+      <Layout>
+        <Head>
+          <title>Homepage</title>
+        </Head>
 
-      <h1>Find your next adventure</h1>
-    </Layout>
+        <h1>Find your next adventure</h1>
+      </Layout>
+    </div>
   );
 }
