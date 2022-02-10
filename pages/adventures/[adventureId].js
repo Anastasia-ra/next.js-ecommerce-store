e8777ad8d51ef8d55b400a23a51efe09c74fba43
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-02-10 18:40:01 +0100

    Change style

--- pages/adventures/[adventureId].js
+++ pages/adventures/[adventureId].js
@@ -3,9 +3,59 @@ import Image from 'next/image';
 import { useState } from 'react';
 import Layout from '../../components/Layout';
 import Link from 'next/link';
+import { css } from '@emotion/react';
 // import adventuresDatabase from '../../util/database';
 import { getParsedCookie, setParsedCookie } from '../../util/cookies.js';
 import { getAdventureById } from '../../util/database';
+// import ChangeQuantity from '../../components/ChangeQuantity';
+
+const gridStyle = css`
+  display: grid;
+  margin-right: 4rem;
+  margin-left: 4rem;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+`;
+
+const textStyle = css`
+  grid-column: 2 / 3;
+  grid-row: 1 / 2;
+`;
+
+const buttonCartStyle = css`
+  /* grid-column: 2 / 3;
+  grid-row: 2 / 3;
+  justify-self: center;
+  align-self: center; */
+`;
+
+const buttonStyle = css`
+  grid-column: 2 / 3;
+  grid-row: 2 / 3;
+  justify-self: start;
+  align-self: center;
+`;
+
+const imageStyle = css`
+  margin-top: 1rem;
+  grid-column: 1 / 2;
+  grid-row: 1 / 3;
+`;
+
+const backLinkStyle = css`
+  // display: block;
+`;
+
+const buttonQuantityStyle = css`
+  border: grey;
+  border-style: solid;
+  display: inline-block;
+  margin: 10px;
+`;
+
+const buttonPlusStyle = css``;
+
+const buttonMinusStyle = css``;
 
 export default function SingleAdventure(props) {
   const [cartList, setCartList] = useState(props.cart);
@@ -31,11 +81,11 @@ export default function SingleAdventure(props) {
     return addedObject.id === props.adventure.id;
   });
 
-  const currentAdventureObject = cartList.find(
+  const currentAdventure = cartList.find(
     (cookieObject) => cookieObject.id === props.adventure.id,
   );
 
-  console.log('currentAdventureObject', currentAdventureObject);
+  console.log('currentAdventureObject', currentAdventure);
 
   function quantityCountUp() {
     const cookieValue = getParsedCookie('cart') || [];
@@ -73,26 +123,56 @@ export default function SingleAdventure(props) {
       <Head>
         <title>{`${props.adventure.name} - ${props.adventure.duration} days`}</title>
       </Head>
-      <h1>{`${props.adventure.duration} days ${props.adventure.name}`}</h1>
-      <Link href="/adventures">
-        <a>Back to all adventures</a>
-      </Link>
-      <Image
-        src={`/adventures/resized-singlePage/${props.adventure.id}.jpg`}
-        width="500"
-        height="333"
-      />
-      <div data-test-id="product-price"> Price: {props.adventure.price}</div>
-      <button onClick={() => toggleAdventureCart(props.adventure.id)}>
-        {adventureIsAdded ? 'Remove from cart' : 'Add to cart'}
-      </button>
-      {currentAdventureObject && (
-        <div>
-          <button onClick={() => quantityCountUp()}>+ </button>
-          {currentAdventureObject.quantity}
-          <button onClick={() => quantityCountDown()}>- </button>
+      <h1>{`${props.adventure.name}`}</h1>
+      <div css={backLinkStyle}>
+        <Link href="/adventures">
+          <a>Back to all adventures</a>
+        </Link>
+      </div>
+      <div css={gridStyle}>
+        <div css={imageStyle}>
+          <Image
+            data-test-id="product-image"
+            src={`/adventures/resized-singlePage/${props.adventure.id}.jpg`}
+            width="500"
+            height="333"
+            css={imageStyle}
+          />{' '}
         </div>
-      )}
+        <div css={textStyle}>
+          <div data-test-id="product-price">
+            {' '}
+            Price: {props.adventure.price}
+            <br />
+            {props.adventure.description}
+          </div>
+        </div>
+        <div css={buttonStyle}>
+          <button
+            css={buttonCartStyle}
+            data-test-id="product-add-to-cart"
+            onClick={() => toggleAdventureCart(props.adventure.id)}
+          >
+            {adventureIsAdded ? 'Remove from cart' : 'Add to cart'}
+          </button>
+          <div css={buttonQuantityStyle}>
+            {currentAdventure && (
+              <div data-test-id="product-quantity">
+                <button css={buttonPlusStyle} onClick={() => quantityCountUp()}>
+                  +{' '}
+                </button>
+                {currentAdventure.quantity}
+                <button
+                  css={buttonMinusStyle}
+                  onClick={() => quantityCountDown()}
+                >
+                  -{' '}
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
