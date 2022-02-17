e1eddb0ddf3f5047dbd8e0fa7e1ab4bb99520ad4
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-02-17 18:21:56 +0100

    Update text descriptions

--- migrations/1644844804-insertAdventures.js
+++ migrations/1644844804-insertAdventures.js
@@ -4,35 +4,39 @@ const adventures = [
     type: 'Cruise',
     duration: 20,
     price: 50,
-    description: 'Meet at least six penguin species',
+    description:
+      'Take a voyage to the frozen south on a polar expedition. Sail across the renowned Drake Passage to reach the Great White Continent. Cruise amongst immense icebergs in search of wildlife like penguin, seal and whale.',
   },
   {
     name: 'Himalaya Trek',
     type: 'Hike',
     duration: 14,
     price: 20,
-    description: 'Cross the Himalayas in two weeks',
+    description:
+      ' These snow-capped mountains have pulled explorers under their spell for centuries, promising an ever-changing landscape of remote villages, thick alpine forests, and rushing rivers set against the otherworldly backdrop of peaks that scrape the belly of heaven itself. Prepare to be astounded.',
   },
   {
     name: 'Ultimate Moon Hike',
     type: 'Hike',
     duration: 50,
     price: 400,
-    description: 'Nice view. Amazing wildlife',
+    description:
+      'Discover the magnificent landscapes of the moon, its exceptional biodiversity and the warm welcome of its inhabitants.',
   },
   {
     name: 'In the jungles of Brazil',
     type: 'Hike',
     duration: 10,
     price: 30,
-    description: 'Meeting the crocodiles',
+    description:
+      'Enjoy exploring the uninhabited wilderness, hiking through the flood plains and immerse yourself in the rainforest as you explore on foot and by canoe. This is a true expedition that goes deep into the forest.',
   },
   {
     name: 'Submarine tour of the earth',
     type: 'Cruise',
     duration: 70,
     price: 250,
-    description: `It is so quiet down here. You'll want to stay forever.`,
+    description: `Tour the Earth's oceans on board our submarine. It is so quiet down there. You'll want to stay forever.`,
   },
 ];
 
