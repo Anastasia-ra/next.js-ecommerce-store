bb5048df2d498872099327283f4e2fbfc856ef26
Anastasia Rambaud <anastasia.rambaud@gmail.com>
2022-02-22 14:07:33 +0100

    Update config file for E2E test

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,9 +1,9 @@
 const config = {
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['<rootDir>/integration'],
-  // preset: 'jest-puppeteer',
-  // testTimeout: 1000000,
-  // testPathIgnorePatterns: ['<rootDir>/util/__tests__'],
+  // testEnvironment: 'jsdom',
+  // testPathIgnorePatterns: ['<rootDir>/integration'],
+  preset: 'jest-puppeteer',
+  testTimeout: 1000000,
+  testPathIgnorePatterns: ['<rootDir>/util/__tests__'],
 };
 
 export default config;
